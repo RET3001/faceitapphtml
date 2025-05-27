50c8a9f024ce5033f8a7b05ca83b95e1d430d814
RET3001 <retgaming31@gmail.com>
2025-05-27 15:57:56 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function addTask() {
-    const input = document.getElementById("taskInput");
-    const task = input.value.trim();
-    if (task === "") return;
-
-    const tg = window.Telegram.WebApp;
-    tg.sendData(task); // Отправка в бота
-    tg.close();
-}
